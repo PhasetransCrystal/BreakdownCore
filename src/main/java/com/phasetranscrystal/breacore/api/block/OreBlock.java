f46467a1a00222c914db540848e6034536dc4985
LeeQianXi <qianxilee232@163.com>
2025-12-03 21:06:40 +0800

    完成部分方块注册 完成矿石注册

---
+++ src/main/java/com/phasetranscrystal/breacore/api/block/OreBlock.java
@@ -0,0 +1,36 @@
+package com.phasetranscrystal.breacore.api.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+
+import com.phasetranscrystal.breacore.api.material.Material;
+import com.phasetranscrystal.breacore.api.tag.TagPrefix;
+import com.phasetranscrystal.brealib.utils.BreaUtil;
+import org.jetbrains.annotations.NotNull;
+
+public class OreBlock extends MaterialBlock {
+
+    public OreBlock(Properties properties, TagPrefix tagPrefix, Material material, boolean registerModel) {
+        super(properties, tagPrefix, material, false);
+        if (registerModel && BreaUtil.isClientSide()) {
+            // OreBlockRenderer.create(this);
+        }
+    }
+
+    @Override
+    protected @NotNull InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos,
+                                                        Player player, BlockHitResult hitResult) {
+        return InteractionResult.PASS;
+    }
+
+    @Override
+    protected InteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        return InteractionResult.PASS;
+    }
+}
--- src/main/java/com/phasetranscrystal/breacore/data/blocks/BreaBlocks.java
+++ src/main/java/com/phasetranscrystal/breacore/data/blocks/BreaBlocks.java
@@ -1,7 +1,15 @@
 package com.phasetranscrystal.breacore.data.blocks;
 
+import net.minecraft.client.data.models.model.TextureSlot;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.neoforge.common.Tags;
 
+import com.google.common.collect.ImmutableTable;
 import com.google.common.collect.Table;
 import com.phasetranscrystal.breacore.api.material.ItemMaterialData;
 import com.phasetranscrystal.breacore.api.material.Material;
@@ -10,6 +18,8 @@ import com.phasetranscrystal.breacore.api.tag.TagPrefix;
 import com.phasetranscrystal.breacore.common.block.StoneBlockType;
 import com.phasetranscrystal.breacore.common.block.StoneTypes;
 import com.phasetranscrystal.breacore.data.items.MaterialItems;
+import com.phasetranscrystal.breacore.data.misc.BreaCreativeModeTabs;
+import com.phasetranscrystal.breacore.mixin.neoforge.BlockPropertiesAccessor;
 import com.phasetranscrystal.breacore.utils.memoization.CacheMemoizer;
 import com.tterrag.registrate.builders.BlockBuilder;
 import com.tterrag.registrate.util.entry.BlockEntry;
@@ -18,9 +28,22 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Supplier;
 
+import static com.phasetranscrystal.breacore.common.registry.BreaRegistration.REGISTRATE;
+
 public class BreaBlocks {
 
-    public static void init() {}
+    public static void init() {
+        // Decor Blocks
+        generateStoneBlocks();
+
+        // Procedural Blocks
+        REGISTRATE.creativeModeTab(() -> BreaCreativeModeTabs.MATERIAL_BLOCK);
+        BreaMaterialBlocks.generateMaterialBlocks();   // Compressed Blocks
+        BreaMaterialBlocks.generateOreBlocks();        // Ore Blocks
+        BreaMaterialBlocks.MATERIAL_BLOCKS = BreaMaterialBlocks.MATERIAL_BLOCKS_BUILDER.build();
+
+        BreaMaterialBlocks.MATERIAL_BLOCKS_BUILDER = null;
+    }
 
     public static Table<StoneBlockType, StoneTypes, BlockEntry<Block>> STONE_BLOCKS;
 
@@ -36,5 +59,93 @@ public class BreaBlocks {
             });
             return builder;
         };
+    }
+
+    public static void generateStoneBlocks() {
+        // Stone type blocks
+        ImmutableTable.Builder<StoneBlockType, StoneTypes, BlockEntry<Block>> builder = ImmutableTable.builder();
+        for (StoneTypes strata : StoneTypes.values()) {
+            if (!strata.generateBlocks) continue;
+            for (StoneBlockType type : StoneBlockType.values()) {
+                String blockId = type.blockId.formatted(strata.getSerializedName());
+                if (BuiltInRegistries.BLOCK.containsKey(ResourceLocation.parse(blockId))) continue;
+                var entry = REGISTRATE.block(blockId, Block::new)
+                        .initialProperties(() -> Blocks.STONE)
+                        .properties(p -> p.strength(type.hardness, type.resistance).mapColor(strata.mapColor))
+                        .transform(type == StoneBlockType.STONE ?
+                                BreaBlocks.unificationBlock(strata.getTagPrefix(), strata.getMaterial()) :
+                                builder2 -> builder2)
+                        .tag(BlockTags.MINEABLE_WITH_PICKAXE, Tags.Blocks.NEEDS_WOOD_TOOL)
+                        .loot((tables, block) -> {
+                            if (type == StoneBlockType.STONE) {
+                                tables.add(block, tables.createSingleItemTableWithSilkTouch(block,
+                                        STONE_BLOCKS.get(StoneBlockType.COBBLE, strata).get()));
+                            } else {
+                                tables.add(block, tables.createSingleItemTable(block));
+                            }
+                        })
+                        .item()
+                        .build();
+                if (type == StoneBlockType.STONE && strata.isNatural()) {
+                    entry.tag(BlockTags.STONE_ORE_REPLACEABLES, BlockTags.BASE_STONE_OVERWORLD,
+                            BlockTags.DRIPSTONE_REPLACEABLE, BlockTags.MOSS_REPLACEABLE);
+                    // .blockstate(GTModels.randomRotatedModel(GTCEu.id(ModelProvider.BLOCK_FOLDER + "/stones/" +
+                    // strata.getSerializedName() + "/" + type.id));
+                } else {
+                    entry.blockstate(() -> (ctx, prov) -> {
+                        prov.getBuilder()
+                                .texture(TextureSlot.ALL, prov.modLoc("block/stones/" + strata.getSerializedName() + "/" + type.id))
+                                .build(ctx.getEntry());
+                    });
+                }
+                if (type == StoneBlockType.STONE) {
+                    entry.tag(Tags.Blocks.STONES);
+                }
+                if (type == StoneBlockType.COBBLE) {
+                    entry.tag(Tags.Blocks.COBBLESTONES);
+                }
+                builder.put(type, strata, entry.register());
+            }
+        }
+        STONE_BLOCKS = builder.build();
+    }
+
+    /**
+     * kinda nasty block property copy function because one doesn't exist.
+     *
+     * @param props the props to copy
+     * @return a shallow copy of the block properties like {@link BlockBehaviour.Properties#ofFullCopy(BlockBehaviour)}
+     *         does
+     */
+    public static BlockBehaviour.Properties copy(BlockBehaviour.Properties props, BlockBehaviour.Properties newProps) {
+        if (props == null) {
+            return newProps;
+        }
+        newProps.destroyTime(((BlockPropertiesAccessor) props).getDestroyTime());
+        newProps.explosionResistance(((BlockPropertiesAccessor) props).getExplosionResistance());
+        if (!((BlockPropertiesAccessor) props).isHasCollision()) newProps.noCollission();
+        if (((BlockPropertiesAccessor) props).isIsRandomlyTicking()) newProps.randomTicks();
+        newProps.lightLevel(((BlockPropertiesAccessor) props).getLightEmission());
+        newProps.mapColor(((BlockPropertiesAccessor) props).getMapColor());
+        newProps.sound(((BlockPropertiesAccessor) props).getSoundType());
+        newProps.friction(((BlockPropertiesAccessor) props).getFriction());
+        newProps.speedFactor(((BlockPropertiesAccessor) props).getSpeedFactor());
+        if (((BlockPropertiesAccessor) props).isDynamicShape()) newProps.dynamicShape();
+        if (!((BlockPropertiesAccessor) props).isCanOcclude()) newProps.noOcclusion();
+        if (((BlockPropertiesAccessor) props).isIsAir()) newProps.air();
+        if (((BlockPropertiesAccessor) props).isIgnitedByLava()) newProps.ignitedByLava();
+        if (((BlockPropertiesAccessor) props).isLiquid()) newProps.liquid();
+        if (((BlockPropertiesAccessor) props).isForceSolidOff()) newProps.forceSolidOff();
+        if (((BlockPropertiesAccessor) props).isForceSolidOn()) newProps.forceSolidOn();
+        newProps.pushReaction(((BlockPropertiesAccessor) props).getPushReaction());
+        if (((BlockPropertiesAccessor) props).isRequiresCorrectToolForDrops()) newProps.requiresCorrectToolForDrops();
+        ((BlockPropertiesAccessor) newProps).setOffsetFunction(((BlockPropertiesAccessor) props).getOffsetFunction());
+        if (!((BlockPropertiesAccessor) props).isSpawnTerrainParticles()) newProps.noTerrainParticles();
+        ((BlockPropertiesAccessor) newProps)
+                .setRequiredFeatures(((BlockPropertiesAccessor) props).getRequiredFeatures());
+        newProps.emissiveRendering(((BlockPropertiesAccessor) props).getEmissiveRendering());
+        newProps.instrument(((BlockPropertiesAccessor) props).getInstrument());
+        if (((BlockPropertiesAccessor) props).isReplaceable()) newProps.replaceable();
+        return newProps;
     }
 }
