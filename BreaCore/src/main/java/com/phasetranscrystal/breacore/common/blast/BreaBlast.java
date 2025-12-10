00f71e2aea388ff871be8723e758f90ed366ee55
LeeQianXi <qianxilee232@163.com>
2025-12-10 18:59:45 +0800

    fix

--- BreaCore/src/main/java/com/phasetranscrystal/breacore/common/blast/BreaBlast.java
+++ BreaCore/src/main/java/com/phasetranscrystal/breacore/common/blast/BreaBlast.java
@@ -1,6 +1,5 @@
 package com.phasetranscrystal.breacore.common.blast;
 
-import com.phasetranscrystal.brealib.BreaLib;
 import com.phasetranscrystal.brealib.utils.BreaUtil;
 
 import com.phasetranscrystal.breacore.api.BreaAPI;
@@ -9,25 +8,14 @@ import com.phasetranscrystal.breacore.api.blast.player.SkillDataSynPacket;
 import com.phasetranscrystal.breacore.api.blast.player.SkillGroup;
 import com.phasetranscrystal.breacore.api.blast.skill.Skill;
 import com.phasetranscrystal.breacore.api.blast.skill.SkillData;
-import com.phasetranscrystal.brealib.utils.BreaUtil;
-
 import com.phasetranscrystal.breacore.api.registry.BreaRegistries;
 
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.Vec3;
 
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -35,7 +23,6 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.attachment.AttachmentType;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
-import net.neoforged.neoforge.event.tick.EntityTickEvent;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
@@ -74,62 +61,6 @@ public class BreaBlast {
                 () -> new RangedAttribute("attribute.name.skill.max_charge", 1, 1, Integer.MAX_VALUE));
 
         EMPTY_SKILL = BreaRegistries.SKILLS.register(BreaUtil.byPath("blast/empty"), Skill.EMPTY);
-        TEST_SKILL = BreaRegistries.SKILLS.register(BreaUtil.byPath("blast/test"),
-                Skill.Builder.<Player>of(30, "inactive")
-                        .start(data -> data.getEntity().displayClientMessage(Component.literal("TestSkillInit"), false))
-                        // .flag(Skill.Flag.INSTANT_COMPLETE, true)
-                        .onEvent(EntityTickEvent.Post.class, (event, data) -> {
-                            Player player = data.getEntity();
-                            if (player.level().getGameTime() % 100 == 0 && player.getHealth() < player.getMaxHealth()) {
-                                player.displayClientMessage(Component.literal("You are healed!"), false);
-                                player.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 100, 2));
-                            }
-                        })
-                        .inactive(builder -> builder
-                                // 按键监听测试
-                                .onHurt((event, data) -> data.addEnergy(-1))
-                                .onAttack((event, data) -> data.addEnergy(2))
-                                .onKillTarget((event, data) -> data.addEnergy(5))
-                                .energyChanged((data, i) -> {
-                                    data.getEntity().displayClientMessage(Component.literal("Energy " + (i >= 0 ? "§a+" : "§c-") + i), true);
-                                })
-                                .chargeChanged((data, i) -> {
-                                    data.getEntity().displayClientMessage(Component.literal("Charge " + (i >= 0 ? "§a+" : "§c-") + i), true);
-                                    ResourceLocation location = ResourceLocation.fromNamespaceAndPath(BreaLib.Core_ID, "blast/skill_test");
-                                    data.addAutoCleanAttribute(new AttributeModifier(location, 0.5 * data.getCharge(), AttributeModifier.Operation.ADD_VALUE), Attributes.MOVEMENT_SPEED);
-                                })
-                                .onChargeReady(data -> data.getEntity().displayClientMessage(Component.literal("ReachReady!"), false))
-                                .onChargeFull(data -> data.getEntity().displayClientMessage(Component.literal("ReachStop!"), false))
-                                .endWith(data -> {
-                                    data.putCacheData("charge_consume", data.getCharge() + 1 + "", true, true);
-                                }))
-                        .judge((data, name) -> !"active".equals(name) || (data.getEntity().level().isDarkOutside() && data.getCharge() >= 1))
-                        .active(builder -> builder
-                                .startWith(data -> {
-                                    Vec3 pos = data.getEntity().position();
-                                    ((ServerLevel) data.getEntity().level()).sendParticles(ParticleTypes.EXPLOSION, pos.x, pos.y, pos.z, 4, 0.5, 0.5, 0.5, 0.5);
-                                    data.setForegroundEnergy(40);
-                                })
-                                .onTick((event, data) -> {
-                                    data.getEntity().displayClientMessage(Component.literal("activeTick:" + data.getForegroundEnergy()), true);
-                                    data.addEnergy(-1);
-                                })
-                                .endWith(data -> {
-                                    data.getEntity().jumpFromGround();
-                                    data.getEntity().addDeltaMovement(new Vec3(0, 0.1, 0));
-                                    data.getEntity().addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 200 * data.getCacheDataAsInt("charge_consume", 0, true), 2));
-                                }))
-                        .onBehaviorChange((data, behavior) -> {
-                            if (data.getActiveTimes() == 5) data.requestDisable();
-                            else {
-                                data.getEntity().displayClientMessage(Component.literal("StateChanged: to \"" + behavior + "\", times: " + data.getActiveTimes()), false);
-                                if ("active".equals(behavior)) data.consumeCharge();
-                            }
-                        })
-                        .onEnd(data -> data.getEntity().displayClientMessage(Component.literal("skill disabled"), false))
-                        .build(Player.class)
-
-        );
     }
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
@@ -162,6 +93,8 @@ public class BreaBlast {
     }
 
     public static void bootstrap() {
+        BreaBlastTest.bootstrap();
+
         BreaRegistries.SKILLS.freeze();
         BreaAPI.postRegisterEvent(BreaRegistries.SKILLS);
     }
--- BreaCore/src/main/java/com/phasetranscrystal/breacore/common/blast/BreaBlastTest.java
+++ BreaCore/src/main/java/com/phasetranscrystal/breacore/common/blast/BreaBlastTest.java
@@ -1,10 +1,11 @@
 package com.phasetranscrystal.breacore.common.blast;
 
-import com.phasetranscrystal.breacore.api.blast.player.SkillGroup;
+import com.phasetranscrystal.brealib.BreaLib;
+import com.phasetranscrystal.brealib.utils.BreaUtil;
 
+import com.phasetranscrystal.breacore.api.blast.player.SkillGroup;
+import com.phasetranscrystal.breacore.api.blast.skill.Skill;
 import com.phasetranscrystal.breacore.api.registry.BreaRegistries;
-import com.phasetranscrystal.breacore.common.blast.player.SkillGroup;
-import com.phasetranscrystal.breacore.common.blast.skill.Skill;
 
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
@@ -13,11 +14,15 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
-import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.neoforge.event.tick.EntityTickEvent;
@@ -25,14 +30,64 @@ import net.neoforged.neoforge.event.tick.EntityTickEvent;
 public class BreaBlastTest {
 
     public static void bootstrap() {
-        if (BreaUtil.isDev()) {
-            SKILL.register(BreaCore.getModEventBus());
+        NeoForge.EVENT_BUS.addListener(BreaBlastTest::testSkillInit);
 
-            // bus.addListener(BreaBlastTest::testSkillInit);
-        }
+        BreaBlast.TEST_SKILL = BreaRegistries.SKILLS.register(BreaUtil.byPath("blast/test"),
+                Skill.Builder.<Player>of(30, "inactive")
+                        .start(data -> data.getEntity().displayClientMessage(Component.literal("TestSkillInit"), false))
+                        // .flag(Skill.Flag.INSTANT_COMPLETE, true)
+                        .onEvent(EntityTickEvent.Post.class, (event, data) -> {
+                            Player player = data.getEntity();
+                            if (player.level().getGameTime() % 100 == 0 && player.getHealth() < player.getMaxHealth()) {
+                                player.displayClientMessage(Component.literal("You are healed!"), false);
+                                player.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 100, 2));
+                            }
+                        })
+                        .inactive(builder -> builder
+                                // 按键监听测试
+                                .onHurt((event, data) -> data.addEnergy(-1))
+                                .onAttack((event, data) -> data.addEnergy(2))
+                                .onKillTarget((event, data) -> data.addEnergy(5))
+                                .energyChanged((data, i) -> {
+                                    data.getEntity().displayClientMessage(Component.literal("Energy " + (i >= 0 ? "§a+" : "§c-") + i), true);
+                                })
+                                .chargeChanged((data, i) -> {
+                                    data.getEntity().displayClientMessage(Component.literal("Charge " + (i >= 0 ? "§a+" : "§c-") + i), true);
+                                    ResourceLocation location = ResourceLocation.fromNamespaceAndPath(BreaLib.Core_ID, "blast/skill_test");
+                                    data.addAutoCleanAttribute(new AttributeModifier(location, 0.5 * data.getCharge(), AttributeModifier.Operation.ADD_VALUE), Attributes.MOVEMENT_SPEED);
+                                })
+                                .onChargeReady(data -> data.getEntity().displayClientMessage(Component.literal("ReachReady!"), false))
+                                .onChargeFull(data -> data.getEntity().displayClientMessage(Component.literal("ReachStop!"), false))
+                                .endWith(data -> {
+                                    data.putCacheData("charge_consume", data.getCharge() + 1 + "", true, true);
+                                }))
+                        .judge((data, name) -> !"active".equals(name) || (data.getEntity().level().isDarkOutside() && data.getCharge() >= 1))
+                        .active(builder -> builder
+                                .startWith(data -> {
+                                    Vec3 pos = data.getEntity().position();
+                                    ((ServerLevel) data.getEntity().level()).sendParticles(ParticleTypes.EXPLOSION, pos.x, pos.y, pos.z, 4, 0.5, 0.5, 0.5, 0.5);
+                                    data.setForegroundEnergy(40);
+                                })
+                                .onTick((event, data) -> {
+                                    data.getEntity().displayClientMessage(Component.literal("activeTick:" + data.getForegroundEnergy()), true);
+                                    data.addEnergy(-1);
+                                })
+                                .endWith(data -> {
+                                    data.getEntity().jumpFromGround();
+                                    data.getEntity().addDeltaMovement(new Vec3(0, 0.1, 0));
+                                    data.getEntity().addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 200 * data.getCacheDataAsInt("charge_consume", 0, true), 2));
+                                }))
+                        .onBehaviorChange((data, behavior) -> {
+                            if (data.getActiveTimes() == 5) data.requestDisable();
+                            else {
+                                data.getEntity().displayClientMessage(Component.literal("StateChanged: to \"" + behavior + "\", times: " + data.getActiveTimes()), false);
+                                if ("active".equals(behavior)) data.consumeCharge();
+                            }
+                        })
+                        .onEnd(data -> data.getEntity().displayClientMessage(Component.literal("skill disabled"), false))
+                        .build(Player.class));
     }
 
-    @SubscribeEvent
     public static void testSkillInit(EntityJoinLevelEvent event) {
         if (event.getEntity() instanceof ServerPlayer serverPlayer) {
             SkillGroup data = serverPlayer.getData(BreaBlast.PLAYER_SKILL_GROUP);
